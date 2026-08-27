1ccfb4183bb4d8ac22826d772e83e8fdb69cd37a
msantos7gabriel <gabriel.msantos7@hotmail.com>
2026-08-27 19:57:10 -0300

    fix: correct sidebar link activation logic and add object dimensions

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,17 @@ const sidebar_pages_links = Array.from(
 function sidebar_changes(event, id) {
   sidebar_pages_links.map((page_link) => {
     page_link.classList.remove("is_active");
+    page_link.childNodes[1].data = page_link.childNodes[1].data.replace(
+      "_active",
+      "_deactive",
+    );
   });
 
-  let pagina = document.getElementById(`link_${id}`).classList.add("is_active");
+  let pagina = document.getElementById(`link_${id}`);
+  pagina.classList.add("is_active");
+  pagina.childNodes[1].data = pagina.childNodes[1].data.replace(
+    "_deactive",
+    "_active",
+  );
+  console.log(pagina.childNodes[1].data);
 }
